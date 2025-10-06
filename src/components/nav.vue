5d2f1cf84ae9553d5bfa368f138164125037a5f3
phanukorn3 <66070155@kmitl.ac.th>
2025-10-06 16:54:07 +0700

    add fetch profile and mypet

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -2,27 +2,43 @@
 import Logo from "@/components/logo.vue";
 import UserProfile from "@/assets/user_profile.png";
 import { useRouter } from "vue-router";
+import { ref, onMounted } from "vue";
+import axios from "axios";
 
 const defaultImage = UserProfile;
+const router = useRouter();
 
 // ต้องมาแก้ด้วย **************
-const username = "Bibi"; // test ชื่อ
-
-const router = useRouter();
 
 function handleProfileChange(event) {
   const value = event.target.value;
   if (!value) return;
-
+  
   if (value === "profile") {
     router.push("/profile");
   } else if (value === "logout") {
     router.push("/login");
   }
-
+  
   event.target.value = "";
 }
+const user = ref({
+  profileImageUrl: "", // จะได้จาก backend
+  name: "", // จะได้จาก backend
+});
+
+// fecth ค่าจาก backend *************
+onMounted(async () => {
+  try {
+    const res = await axios.get("/api/profile");  // จาก backend
+    user.value = res.data;
+  } catch (err) {
+    console.error("Failed to fetch user:", err);
+  }
+});
+
 </script>
+
 <template>
   <nav class="bg-[#FFF7E2]/62 p-4 shadow-md w-full h-18 rounded-2xl">
     <div class="container mx-auto flex items-center h-11">
@@ -30,26 +46,44 @@ function handleProfileChange(event) {
 
       <!-- border-2 border-gray-300 bg-[#FFF7E2] rounded-3xl p-1.5 -->
       <div class="flex space-x-4 ml-75">
-        <router-link to="/" class="text-[#B26D38] text-xl mr-8"
-          >Overview</router-link
-        >
-        <router-link to="/mypet" class="text-[#B26D38] text-xl"
-          >My Pet</router-link
-        >
+        <router-link to="/" v-slot="{ isActive }">
+          <span
+            :class="[
+              'text-[#B26D38] text-xl mr-8 px-3 py-1.5 rounded-3xl transition-all duration-300 hover:bg-[#FFF7E2]',
+              isActive
+                ? 'border-2 border-gray-300 bg-[#FFF7E2]'
+                : 'border-2 border-transparent',
+            ]"
+          >
+            Overview
+          </span>
+        </router-link>
+
+        <router-link to="/mypet" v-slot="{ isActive }">
+          <span
+            :class="[
+              'text-[#B26D38] text-xl px-3 py-1.5 rounded-3xl transition-all duration-300 hover:bg-[#FFF7E2]',
+              isActive
+                ? 'border-2 border-gray-300 bg-[#FFF7E2]'
+                : 'border-2 border-transparent',
+            ]"
+          >
+            My Pet
+          </span>
+        </router-link>
       </div>
       <div class="ml-auto">
         <!-- <router-link to="/profile"> -->
-          <img
-            :src="defaultImage"
-            alt="Profile"
-            class="w-10 h-10 rounded-full object-cover border-1 border-gray-200"
-        />
-      <!-- </router-link> -->
         <!-- <img
+          :src="defaultImage"
+          alt="Profile"
+          class="w-10 h-10 rounded-full object-cover border-1 border-gray-200" -->
+        <!-- </router-link> -->
+        <img
         :src="user.profileImageUrl || defaultImage"
         alt="Profile"
-        class="w-10 h-10 rounded-full object-cover border-2 border-orange-500"
-       /> -->
+        class="w-10 h-10 rounded-full object-cover border-2 border-gray-200"
+       />
       </div>
       <div>
         <select
@@ -58,7 +92,7 @@ function handleProfileChange(event) {
           class="ml-2 text-[#EA580C] text-lg rounded-md transition-all duration-300 hover:text-[#613818] bg-transparent border-none focus:outline-none"
           @change="handleProfileChange"
         >
-          <option disabled selected hidden>{{ username }}</option>
+          <option disabled selected hidden>{{ user.name }}</option>
           <option value="profile">Profile</option>
           <option value="logout">Logout</option>
         </select>
--- src/pages/mypet.vue
+++ src/pages/mypet.vue
@@ -1,25 +1,22 @@
 <script setup>
 // import api from "@/service/api.js";
 import Nav from "@/components/nav.vue";
-import { ref } from "vue";
-import profilepic from "@/assets/user_profile.png";
+import { ref, onMounted } from "vue";
+import axios from "axios";
 
 // for test เด้อ ***** เปลี่ยนเป็นค่าที่ดึงมาจาก database
-const pets = ref([
-  { id: 1, name: "Milo", image: profilepic },
-  { id: 2, name: "Luna", image: profilepic },
-  { id: 3, name: "Bobby", image: profilepic },
-  { id: 1, name: "Milo", image: profilepic },
-  { id: 2, name: "Luna", image: profilepic },
-  { id: 3, name: "Bobby", image: profilepic },
-  { id: 3, name: "Bobby", image: profilepic },
-  { id: 3, name: "Bobby", image: profilepic },
-  { id: 3, name: "Bobby", image: profilepic },
-  { id: 3, name: "Bobby", image: profilepic },
-  { id: 3, name: "Bobby", image: profilepic },
-  { id: 3, name: "Bobby", image: profilepic },
-]);
+
+const pets = ref([]);
+onMounted(async () => {
+  try {
+    const res = await axios.get("/api/pets"); // เปลี่ยนเป็น endpoint จริง รอต้า
+    pets.value = res.data;
+  } catch (err) {
+    console.error("Failed to fetch pets:", err);
+  }
+});
 </script>
+
 <template>
   <div class="relative min-h-screen flex items-center justify-center">
     <!-- รูปพื้นหลัง -->
@@ -31,7 +28,7 @@ const pets = ref([
     <div class="absolute inset-0 bg-gray-500/70"></div>
 
     <!-- เนื้อหาด้านใน -->
-     <!-- ตรงส่วนนี้ยังไม่ได้ทำให้ scroll ลงได้ ยังหาวิธีทำอยู่ -->
+    <!-- ตรงส่วนนี้ยังไม่ได้ทำให้ scroll ลงได้ ยังหาวิธีทำอยู่ -->
     <div
       class="relative bg-[#F4D9A2] p-8 rounded-2xl shadow-lg flex flex-col w-320 h-200"
     >
